7c168bd74ba487bea98190dc856a413af1d8ec52
Euaks <126408513+Euaks@users.noreply.github.com>
2024-03-17 11:29:36 -0300

    Add files via upload

---
+++ Banco de dados.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE sapit;
+USE sapit;
+CREATE TABLE cadastro_prof (
+	nome VARCHAR(70),
+	email VARCHAR(60),
+	data_nasc INT(20),
+	carga_hr VARCHAR(10),
+	disciplina VARCHAR(30), 
+	senha VARCHAR(30),
+	siape INT(30),
+	instituicao VARCHAR(50),
+	img_prof VARCHAR(255),
+	estatus_trabalho Varchar(20),
+	telefone int(17)
+);
+
+CREATE TABLE _admin (
+	nome VARCHAR(60),
+    cargo VARCHAR(30),
+    cpf VARCHAR(20),
+    siape VARCHAR(30),
+    senha VARCHAR(30)
+    );
+    CREATE TABLE tabela (
+		aulas_sm VARCHAR(30),
+        manutecao_ens VARCHAR(10),
+        atendimento VARCHAR (10),
+        outras_atv VARCHAR (10),
+        extensao VARCHAR(10),
+        pesquisa VARCHAR(10),
+        reuniao VARCHAR(10),
+        gestao VARCHAR(10),
+        comisa VARCHAR(10),
+        comisao VARCHAR(10)
+        );
+        
+    );
